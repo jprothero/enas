73d90f032c5f9ef15ba607ff4dfa08d432514971
John <john.s.prothero@gmail.com>
2018-04-03 15:34:06 -0400

    Trying to add alphazero loss to this

--- src/cifar10/general_controller.py
+++ src/cifar10/general_controller.py
@@ -80,6 +80,8 @@ class GeneralController(Controller):
     self._create_params()
     self._build_sampler()
 
+    self.alphazero = create_alphazero()
+
   def _create_params(self):
     initializer = tf.random_uniform_initializer(minval=-0.1, maxval=0.1)
     with tf.variable_scope(self.name, initializer=initializer):
@@ -118,6 +120,9 @@ class GeneralController(Controller):
               self.w_soft["count"].append(tf.get_variable(
                 "w_count", [self.lstm_size, self.out_filters - 1]));
 
+        with tf.variable_scope("critic"):
+          self.w_critic = tf.get_variable("w", [self.lstm_size, 1])
+
       with tf.variable_scope("attention"):
         self.w_attn_1 = tf.get_variable("w_1", [self.lstm_size, self.lstm_size])
         self.w_attn_2 = tf.get_variable("w_2", [self.lstm_size, self.lstm_size])
@@ -134,8 +139,10 @@ class GeneralController(Controller):
     arc_seq = []
     entropys = []
     log_probs = []
+    probs = []
     skip_count = []
     skip_penaltys = []
+    all_h = []
 
     prev_c = [tf.zeros([1, self.lstm_size], tf.float32) for _ in
               xrange(self.lstm_num_layers)]
@@ -165,18 +172,25 @@ class GeneralController(Controller):
         log_prob = tf.nn.sparse_softmax_cross_entropy_with_logits(
           logits=logit, labels=branch_id)
         log_probs.append(log_prob)
+        probs.append(tf.nn.softmax(logit))
         entropy = tf.stop_gradient(log_prob * tf.exp(-log_prob))
         entropys.append(entropy)
         inputs = tf.nn.embedding_lookup(self.w_emb, branch_id)
       else:
         for branch_id in xrange(self.num_branches):
           next_c, next_h = stack_lstm(inputs, prev_c, prev_h, self.w_lstm)
+          all_h.append(tf.stop_gradient(next_h[-1]))
           prev_c, prev_h = next_c, next_h
           logit = tf.matmul(next_h[-1], self.w_soft["start"][branch_id])
+          #so this is the point where the logit is produced...
+          #the idea is we need to take that logit, use the probabilities to search 
+          #for the next option, etc.
+          #so do do this I think we might need to experiment with putting this stuff in functions
           if self.temperature is not None:
             logit /= self.temperature
           if self.tanh_constant is not None:
             logit = self.tanh_constant * tf.tanh(logit)
+          #okay so this is where the logit
           start = tf.multinomial(logit, 1)
           start = tf.to_int32(start)
           start = tf.reshape(start, [1])
@@ -184,9 +198,25 @@ class GeneralController(Controller):
           log_prob = tf.nn.sparse_softmax_cross_entropy_with_logits(
             logits=logit, labels=start)
           log_probs.append(log_prob)
+          probs.append(tf.nn.softmax(logit))
           entropy = tf.stop_gradient(log_prob * tf.exp(-log_prob))
           entropys.append(entropy)
           inputs = tf.nn.embedding_lookup(self.w_emb["start"][branch_id], start)
+
+          #so how would this change.... as of right now the net just predicts
+          #what it would do for each decision, and it uses some losses on them
+          #alphazero uses an mcts structure to look into the future and
+          #use the policy and value to make a stronger prediction about what to do
+          #so basically I need to replace the existing loss with an MCTS loss,
+          #and I need to replace the existing decisions with MCTS decisions
+
+          #I could probably for now just have the alpha zero be a policy improvement
+          #i.e. it would just be a policy loss function and we wouldn't care
+          #about using the improved search probabilities immediately, which would be
+          #more complicated. 
+          #so basically we would just defined an alphazero structure here 
+          #get the improved search probabilities, and do a cross entropy loss
+          #between them and the log probabilities
 
           next_c, next_h = stack_lstm(inputs, prev_c, prev_h, self.w_lstm)
           prev_c, prev_h = next_c, next_h
@@ -199,6 +229,13 @@ class GeneralController(Controller):
           mask = tf.reshape(mask, [1, self.out_filters - 1])
           mask = tf.less_equal(mask, self.out_filters-1 - start)
           logit = tf.where(mask, x=logit, y=tf.fill(tf.shape(logit), -np.inf))
+          #so lets assume that logit at this is batch_size, out_filters
+          #so in theory we can pass the logit to a softmax and sent it to the alphazero
+          #function. 
+
+          self.probas = tf.nn.softmax(logit)
+          self.all_h = all_h
+
           count = tf.multinomial(logit, 1)
           count = tf.to_int32(count)
           count = tf.reshape(count, [1])
@@ -206,6 +243,7 @@ class GeneralController(Controller):
           log_prob = tf.nn.sparse_softmax_cross_entropy_with_logits(
             logits=logit, labels=count)
           log_probs.append(log_prob)
+          probs.append(tf.nn.softmax(logit))
           entropy = tf.stop_gradient(log_prob * tf.exp(-log_prob))
           entropys.append(entropy)
           inputs = tf.nn.embedding_lookup(self.w_emb["count"][branch_id], count)
@@ -236,7 +274,7 @@ class GeneralController(Controller):
         log_prob = tf.nn.sparse_softmax_cross_entropy_with_logits(
           logits=logit, labels=skip)
         log_probs.append(tf.reduce_sum(log_prob, keep_dims=True))
-
+        probs.append(tf.nn.softmax(logit))
         entropy = tf.stop_gradient(
           tf.reduce_sum(log_prob * tf.exp(-log_prob), keep_dims=True))
         entropys.append(entropy)
@@ -261,6 +299,8 @@ class GeneralController(Controller):
     log_probs = tf.stack(log_probs)
     self.sample_log_prob = tf.reduce_sum(log_probs)
 
+    self.probs = tf.stack(probs)
+
     skip_count = tf.stack(skip_count)
     self.skip_count = tf.reduce_sum(skip_count)
 
@@ -273,23 +313,98 @@ class GeneralController(Controller):
                       tf.to_float(child_model.batch_size))
     self.reward = self.valid_acc
 
-    normalize = tf.to_float(self.num_layers * (self.num_layers - 1) / 2)
-    self.skip_rate = tf.to_float(self.skip_count) / normalize
+    # normalize = tf.to_float(self.num_layers * (self.num_layers - 1) / 2)
+    # self.skip_rate = tf.to_float(self.skip_count) / normalize
 
-    if self.entropy_weight is not None:
-      self.reward += self.entropy_weight * self.sample_entropy
+    all_h = tf.concat(self.all_h, axis=0)
 
-    self.sample_log_prob = tf.reduce_sum(self.sample_log_prob)
-    self.baseline = tf.Variable(0.0, dtype=tf.float32, trainable=False)
-    baseline_update = tf.assign_sub(
-      self.baseline, (1 - self.bl_dec) * (self.baseline - self.reward))
+    value_function = tf.matmul(all_h, self.w_critic)
+    advantage = value_function - self.reward
+    critic_loss = tf.reduce_sum(advantage ** 2)
 
-    with tf.control_dependencies([baseline_update]):
-      self.reward = tf.identity(self.reward)
+    #so in theory value_function is the values
+    #and we can get the probas through the softmax of the logits
 
-    self.loss = self.sample_log_prob * (self.reward - self.baseline)
-    if self.skip_weight is not None:
-      self.loss += self.skip_weight * self.skip_penaltys
+    #do alphazero stuff here
+
+    # self.alphazero_loss = tf.matmul(search_probas, self.sample_log_probs)
+    # self.loss = 
+
+    class AlphaZero:
+      def __init__(self, num_sims=20):
+        self.num_sims = 20
+
+        self.N = []
+        self.W = []
+        self.U = []
+        self.Q = []
+
+        self.trajectory = []
+      
+      def search(self, probas, values):
+        #so lets see... this is kind of reliant on the assumption that choices are made
+        #sequentially and we can continuously evaluate them with the controller
+        for _ in range(self.num_sims):
+          for b_idx, probas, value in enumerate(zip(probas, values)):
+            np.random.choice(len(probas), p=probas)
+      
+
+
+      for _ in range(num_sims):
+        leaf_node = self.select(root_node)
+
+    self.alphazero.mcts_step(probs, value_function)
+    self.alphazero(probs, value_function)
+
+    
+    self.sample_probs()
+
+    # self.loss = -tf.reduce_mean(self.sample_log_probs * advantage)
+
+    critic_train_step = tf.Variable(
+          0, dtype=tf.int32, trainable=False, name="critic_train_step")
+    critic_train_op, _, _, _ = get_train_ops(
+      critic_loss,
+      [self.w_critic],
+      critic_train_step,
+      clip_mode=None,
+      lr_init=1e-3,
+      lr_dec_start=0,
+      lr_dec_every=int(1e9),
+      optim_algo="adam",
+      sync_replicas=False)
+
+    #so what I need to do is point the input function to alpha zero and run 
+    #a sequence, so that we take a state and we output a policy and value
+
+    # if self.entropy_weight is not None:
+    #   self.reward += self.entropy_weight * self.sample_entropy
+
+    # self.sample_log_prob = tf.reduce_sum(self.sample_log_prob)
+    # self.baseline = tf.Variable(0.0, dtype=tf.float32, trainable=False)
+    #so 0 - 1
+    #so up to -1 loss, i.e. max loss is reward = 0
+    #they scale it then be 
+
+    #hmmm Idk seems like theyre doing a discoutn thing
+    #so maybe the baseline changes over time based on the difference, and 
+    #1 - self.bl_dec is the rate of change
+    # baseline_update = tf.assign_sub(
+    #   self.baseline, (1 - self.bl_dec) * (self.baseline - self.reward))
+
+    # with tf.control_dependencies([baseline_update]):
+    #   self.reward = tf.identity(self.reward)
+
+    #so this is the REINFORCE loss
+    #we want to replace this with alphazero probas and MSE
+    #so the reward is between 0 and 1 and the baseline is too 
+    #1 - 1 = 0
+    #1 - 0 = 1
+    #.7 - 1 = -.3
+    #still dont totally intuitively get this
+    # self.loss = self.sample_log_prob * (self.reward - self.baseline)
+    # if self.skip_weight is not None:
+      # self.loss += self.skip_weight * self.skip_penaltys
 
     self.train_step = tf.Variable(
         0, dtype=tf.int32, trainable=False, name="train_step")
